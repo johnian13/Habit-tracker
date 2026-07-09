ab4c9c64177c9afa1e9d7f0b8bcdc94ea11dc65b
johnian13 <johnianyebra72@gmail.com>
2026-07-09 10:48:39 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,13 @@
 const CACHE_NAME = "orbit-v1";
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
